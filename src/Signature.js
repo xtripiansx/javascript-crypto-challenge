23eee3527b390a713d660a46b166bdec5ea273f2
DE GRAUWE Maxime <maxime.de.grauwe@student.ehb.be>
2019-10-02 19:49:18 +0200

    test

---
+++ src/Signature.js
@@ -0,0 +1,11 @@
+/*
+<script>
+    window.sodium = {
+        onload: function (sodium) {
+            let h = sodium.crypto_generichash(64, sodium.from_string('test'));
+            console.log(sodium.to_hex(h));
+        }
+    };
+</script>
+<script src="sodium.js" async></script>
+*/
